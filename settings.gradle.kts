df2afc590a0e8b29a0126f2eb65c89ce109bb81e
dikpaal <dikpaal@icloud.com>
2026-02-09 06:58:41 -0800

    add gradle.kts file

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "distributed-logging-system"
+
+include("common")
+include("ingestion-service")
+include("processing-service")
+include("monitoring-service")
+include("alert-service")
